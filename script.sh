80760d0bfaf12d7a99f68dd70df710ccd3e2f0bc
KauaHenSilva <kaua.sbc@gmail.com>
2024-06-17 19:02:27 -0300

    refactor: Improve script.sh for Linux execution and package installation

--- script.sh
+++ script.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Função para verificar e instalar dependências
 install_dependency() {
   local command_name="$1"
   local package_name="$2"
 
-  # Verifica se o comando já está instalado
   if ! command -v "$command_name" &>/dev/null; then
     echo "$package_name não está instalado."
     read -p "Deseja instalar $package_name? (S/N): " resposta
@@ -19,14 +17,12 @@ install_dependency() {
   fi
 }
 
-# Verificar e atribuir o sistema operacional
 if [ "$(uname -s)" != "Linux" ]; then
   echo "Este script suporta apenas sistemas operacionais Linux."
   exit 1
 fi
 
 echo "Sistema Operacional Linux detectado."
-
 echo "Escolha o método de execução:"
 echo "1 - Executar localmente"
 echo "2 - Executar usando Docker"
@@ -36,16 +32,13 @@ case "$opcao_execucao" in
 1)
   echo "Executando localmente..."
 
-  # Atualizar os repositórios de pacotes
   echo "Atualizando pacotes no Linux..."
   sudo apt-get update
 
-  # Verificar e instalar as dependências
   install_dependency "meson" "meson"
   install_dependency "ninja" "ninja-build"
   install_dependency "pkg-config" "pkg-config"
 
-  # Verificar se o GTK está instalado
   if ! pkg-config --exists gtk+-3.0; then
     echo "GTK não está instalado."
     read -p "Deseja instalar o GTK? (S/N): " resposta
@@ -58,7 +51,6 @@ case "$opcao_execucao" in
     echo "GTK já está instalado."
   fi
 
-  # Se não existir a pasta builddir, criar
   if [ ! -d builddir ]; then
     echo "Criando setup builddir..."
     meson setup builddir
@@ -67,18 +59,15 @@ case "$opcao_execucao" in
     meson compile -C builddir
   fi
 
-  # Compilar o projeto
   echo "Compilando o projeto..."
   ninja -C builddir
 
-  # Executar o projeto
   echo "Executando o projeto..."
   ./builddir/work_01-TAD-Lista
   ;;
 2)
   echo "Executando usando Docker..."
 
-  # Verificar se o docker está instalado:
   install_dependency "docker" "docker"
 
   sudo docker build -t work_01-tad-lista .
